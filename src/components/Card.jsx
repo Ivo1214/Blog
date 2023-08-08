f1cac2dcac82abdc303d877c310f1040732f4587
Ivo1214 <ivo1214@live.com>
2023-08-08 15:22:08 -0300

    Inicio de proyecto

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Posteos from './Posteos';
+
+function leer(id) {
+    alert(`LEER posteo ID ${id}`);
+}
+
+function modificar (id){
+    alert(`MODIFICAR posteo ID ${id}`);
+}
+
+const Card = () => {
+    return (<div>
+        {
+            Posteos.map((post)=>(
+                <div key={post.id}>
+                    <h1>{post.titulo}</h1>
+                    <button onClick={() => leer(post.id)}>LEER</button>
+                    <button onClick={() => modificar(post.id)}>MODIFICAR</button>
+                </div>
+            ))
+        }
+    </div>)
+};
+
+export default Card;
