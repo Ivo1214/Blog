fd21cc02e935cf88a949350b857b16d667bcc00d
Ivo1214 <ivo1214@live.com>
2023-09-25 16:49:09 -0300

    Navbar usando material ui
    Se cambio la navbar anterior por una de material ui. Falta resolver el logeo del usuario.

--- src/views/nav/Nav.jsx
+++ src/views/nav/Nav.jsx
@@ -1,40 +1,225 @@
+// import React,{useContext} from 'react';
+// import { NavLink } from 'react-router-dom';
+// import { Logeado } from '../../context/Logeado';
+
+// const Nav = () => {
+//     const {currentUser, setCurrentUser} = useContext(Logeado);
+//     return (
+//         <nav>
+//             <ul>
+//                 <li><NavLink to='/' className={({ isActive }) => (isActive ? "active" : "")}>Inicio</NavLink></li>
+//                 <li><NavLink to='/posteos' className={({ isActive }) => (isActive ? "active" : "")}>Posteos</NavLink></li>
+//                 <li><NavLink to='/crear' className={({ isActive }) => (isActive ? "active" : "")}>Crear Posteo</NavLink></li>
+//                 {currentUser.name && currentUser.name !== "" ? (
+//                     <>
+//                         <li>
+//                             <span>{currentUser.name}</span>
+//                         </li>
+//                         <li>
+//                             <NavLink
+//                                 to='/login'
+//                                 className={({ isActive }) => (isActive ? "active" : "")}
+//                                 onClick={() => setCurrentUser({ name: "", email: "" })}
+//                             >
+//                                 Cerrar sesión
+//                             </NavLink>
+//                         </li>
+//                     </>
+//                 ) : (
+//                     <>
+//                         <li>
+//                             <NavLink to='/login' className={({ isActive }) => (isActive ? "active" : "")}>LogIn</NavLink>
+//                         </li>
+//                     </>
+//                 )}
+//             </ul>
+//         </nav>
+//     );
+// };
+
+// export default Nav;
+
 import React,{useContext} from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import {AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import AdbIcon from '@mui/icons-material/Adb';
 import { Logeado } from '../../context/Logeado';
 
-const Nav = () => {
-    const {currentUser, setCurrentUser} = useContext(Logeado);
-    return (
-        <nav>
-            <ul>
-                <li><NavLink to='/' className={({ isActive }) => (isActive ? "active" : "")}>Inicio</NavLink></li>
-                <li><NavLink to='/posteos' className={({ isActive }) => (isActive ? "active" : "")}>Posteos</NavLink></li>
-                <li><NavLink to='/crear' className={({ isActive }) => (isActive ? "active" : "")}>Crear Posteo</NavLink></li>
-                {currentUser.name && currentUser.name !== "" ? (
-                    <>
-                        <li>
-                            <span>{currentUser.name}</span>
-                        </li>
-                        <li>
-                            <NavLink
-                                to='/login'
-                                className={({ isActive }) => (isActive ? "active" : "")}
-                                onClick={() => setCurrentUser({ name: "", email: "" })}
-                            >
-                                Cerrar sesión
-                            </NavLink>
-                        </li>
-                    </>
-                ) : (
-                    <>
-                        <li>
-                            <NavLink to='/login' className={({ isActive }) => (isActive ? "active" : "")}>LogIn</NavLink>
-                        </li>
-                    </>
-                )}
-            </ul>
-        </nav>
-    );
-};
+const pages = ['Inicio', 'Posteos', 'Crear Posteo'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
+function Nav() {
+  const {currentUser, setCurrentUser} = useContext(Logeado);
+  const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = (event) => {
+    let selectedOption = event.target.innerHTML;
+    if (selectedOption.length > 15) {
+        let textLenght = selectedOption.indexOf('<');
+        selectedOption = selectedOption.substring(0, textLenght);
+    }
+
+    switch (selectedOption) {
+        case 'Inicio':
+            navigate("/");
+            break;
+        case 'Posteos':
+            navigate("/posteos");
+            break;
+        case 'Crear Posteo':
+            navigate("/crear");
+            break;
+        default:
+            console.log("Error");
+            break;
+    }
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = (event) => {
+    // console.log(event.target.innerHTML);
+    switch (event.target.innerHTML) {
+        case 'Logout':
+            setCurrentUser({ name: "", email: "" });
+            break;
+        default:
+            break;
+    }
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="sm">
+        <Toolbar disableGutters>
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography> */}
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography> */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
 export default Nav;
