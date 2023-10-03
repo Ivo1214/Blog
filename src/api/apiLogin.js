bf2d38200e4f19b611441b9e39e7f07ddaedf446
Ivo1214 <ivo1214@live.com>
2023-10-03 20:09:05 -0300

    Loggin con API de INOVE

---
+++ src/api/apiLogin.js
@@ -0,0 +1,21 @@
+import {client} from "./client";
+
+export const apiLogin = {
+    post: async function (name, pass) {
+        const datos = {
+            usuario: name.toString(),
+            password: pass.toString()
+        };
+        const response = await client.request({
+            url: '/login',
+            method: 'POST',
+            headers: {'Content-type':'application/x-www-form-urlencoded'},
+            data: datos
+        })
+
+        if (response){
+            console.log(response.data);
+            return response.data;
+        }
+    }
+}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,39 @@
 import React, {useContext} from 'react';
 import { Logeado } from '../context/Logeado';
 import { useNavigate } from 'react-router-dom';
+import {apiLogin} from '../api/apiLogin';
 
 const Login = () => {
     const { currentUser, setCurrentUser } = useContext(Logeado);
     const navigate = useNavigate();
 
-    function HandleSubmit(e) {
+    async function HandleSubmit(e) {
         e.preventDefault();
-
-        const obj = {
-            user: e.target.nombre.value,
+        let name = e.target.name.value;
+        let password = e.target.password.value;
+        
+        apiLogin.post(name, password)
+        .then((response)=>{
+          const obj = {
+            user: name,
             auth: true
-        };
-        setCurrentUser(obj);
-        navigate("/");
+          };
+          setCurrentUser(obj);
+          sessionStorage.setItem('isAuthenticated', true);
+          sessionStorage.setItem('user', name);
+          navigate("/");
+        })
+        .catch((error)=>{
+          alert(error.response.data.detail);
+        });
+        
+        
     };
     return (
         <>
           <form onSubmit={HandleSubmit}>
-            <input type="text" placeholder='Nombre:' name='nombre'/>
-            <input type="email" placeholder='Email:' name='email'/>
+            <input type="text" placeholder='Nombre:' name='name'/>
+            <input type="password" placeholder='Contraseña:' name='password'/>
             <input type="submit" value={"Enviar"}/>
           </form>  
         </>
